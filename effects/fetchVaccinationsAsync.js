f20731b13ca4ff51e374c42160ccd40904b63bde
Brent Vatne <brentvatne@gmail.com>
2016-09-10 17:16:50 -0700

    Remove pointless import

--- effects/fetchVaccinationsAsync.js
+++ effects/fetchVaccinationsAsync.js
@@ -1,6 +1,5 @@
 import { AsyncStorage } from 'react-native';
 import { fromJS, OrderedMap } from 'immutable';
-import { FETCH_VACCINATIONS } from '../constants/actions';
 import fetchVaccinationsSuccess from '../actions/fetchVaccinationsSuccess';
 import fetchVaccinationsFailure from '../actions/fetchVaccinationsFailure';
 import { KEY } from '../constants/storage';
